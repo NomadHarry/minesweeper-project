d49aec41efb2bc0d196322a52b628f3172a31a78
NomadHarry <harry@holloway.digital>
2018-03-21 21:53:43 +0000

    Added In-Play Board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -3,9 +3,17 @@
 
 // Board Lines
 const blankLine = '  |   |  ';
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
 
 // Blank Board
 console.log('This is what an empty board would look like:');
 console.log(blankLine);
 console.log(blankLine);
+console.log(blankLine);
+
+// In-Play Board
+console.log('This is what a board with a guess and a bomb on it would look like:')
+console.log(guessLine);
+console.log(bombLine);
 console.log(blankLine);
