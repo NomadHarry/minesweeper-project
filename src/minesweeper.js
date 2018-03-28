1d28733dab3bbb0d27427d36459f9430992f423a
NomadHarry <harry@holloway.digital>
2018-03-29 00:18:31 +0100

    Generate Board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,4 +1,19 @@
 // Minesweeper Project
 
-const board = [[' '],[' '],[' ']];
-console.log(board);
+// Board Layout
+const board = [
+		[' ',' ',' '],
+		[' ',' ',' '],
+		[' ',' ',' '],
+		];
+
+// Generate Board
+const printBoard = board => {
+	console.log('Current Board: ');
+	console.log(board[0].join(' | '));
+	console.log(board[1].join(' | '));
+	console.log(board[2].join(' | '));
+};
+
+// Print Board
+console.log(printBoard(board));
