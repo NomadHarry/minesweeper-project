27d37f014d7ae07218645f6438791ed57ac755bd
NomadHarry <harry@holloway.digital>
2018-03-28 23:59:41 +0100

    Create Board Structure

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,19 +1,4 @@
 // Minesweeper Project
 
-
-// Board Lines
-const blankLine = '  |   |  ';
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
-
-// Blank Board
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-
-// In-Play Board
-console.log('This is what a board with a guess and a bomb on it would look like:')
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const board = [[' '],[' '],[' ']];
+console.log(board);
